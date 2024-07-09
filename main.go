c7bab54bd86d7f3448f5b3a5798f69593784060d
Tomas <bekjon_98@mail.ru>
2024-07-09 21:38:52 +0300

    update

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
-package redisClient
+package main
 
 import (
 	"fmt"
 	"net"
+	"strconv"
 )
 
 type RedisClient struct {
@@ -30,18 +31,45 @@ func NewClient(host string, port int) *RedisClient {
 }
 
 func (r *RedisClient) Get(key string) []byte {
-	return make([]byte, 0)
+	arr := r.BuildArray(2)
+	arr = r.BuildGet(arr, key)
+	_, err := r.conn.Write(arr)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return arr
 }
 
 func (r *RedisClient) Set(key string, val string) []byte {
 	return make([]byte, 0)
 }
 
-func (r *RedisClient) BuildArray(count int) []byte {
-	return make([]byte, 0)
+func (r *RedisClient) BuildArray(count int64) []byte {
+	arr := make([]byte, 0)
+	arr = append(arr, '*')
+	arr = strconv.AppendInt(arr, count, 10)
+	return append(arr, '\r', '\n')
+}
+
+func (r *RedisClient) BuildGet(arr []byte, key string) []byte {
+	arr = append(arr, []byte("3$")...)
+	arr = append(arr, '\r', '\n')
+	l := len(key)
+	arr = append(arr, '$')
+	arr = strconv.AppendInt(arr, int64(l), 10)
+	arr = append(arr, '\r', '\n')
+	arr = append(arr, []byte(key)...)
+	arr = append(arr, '\r', '\n')
+	fmt.Println(string(arr))
+	return arr
 }
 
 func main() {
 	c := NewClient("localhost", 6379)
-	fmt.Println(c)
+	arr := c.Get("test")
+	fmt.Println(c, string(arr))
+	buf := make([]byte, 512)
+	n, _ := c.conn.Read(buf)
+	fmt.Println("response", string(buf[:n]))
 }
